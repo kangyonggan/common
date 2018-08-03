01713d1dcf8d1f6014c8ba8512991138a2c8662b
kangyonggan <kangyonggan@gmail.com>
2018-08-03 09:53:50 +0800

    app

---
+++ src/main/java/com/kangyonggan/common/App.java
@@ -0,0 +1,26 @@
+package com.kangyonggan.common;
+
+/**
+ * APP常量
+ *
+ * @author kangyonggan
+ * @since 8/1/18
+ */
+public interface App {
+
+    /**
+     * 文章平台
+     */
+    String ARTICLE_PLATFORM = "ARTICLE-PLATFORM";
+
+    /**
+     * 小说平台
+     */
+    String NOVEL_PLATFORM = "NOVEL-PLATFORM";
+
+    /**
+     * 成语平台
+     */
+    String PHRASAL_PLATFORM = "PHRASAL-PLATFORM";
+
+}
